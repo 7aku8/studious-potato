d020ce7c11b16a8043e6de881ee90384c4acc45a
Jakub <jakubwolak.123@gmail.com>
2021-12-21 22:03:14 +0100

    canvas particle animtaion

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,21 @@ canvas.height = canvas.parentElement.clientHeight
 
 const ctx = canvas.getContext('2d')
 
+const mouse = {
+  x: null,
+  y: null,
+  radius: 400
+}
+
+document.onmousemove = ({ x, y }) => {
+  mouse.x = x
+  mouse.y = y
+}
+
 class Particle {
   constructor ({ x, y }) {
     this.x = x
     this.y = y
-
-    this.sx = 0.1
-    this.sy = 0.1
 
     this.dx = Math.random() * 2 - 1
     this.dy = Math.random() * 2 - 1
@@ -20,6 +28,8 @@ class Particle {
   }
 
   draw () {
+    ctx.globalAlpha = 1
+
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
     ctx.fillStyle = 'white'
@@ -33,22 +43,41 @@ class Particle {
     this.x += this.dx
     this.y += this.dy
 
+    const { distance, dx, dy } = this.mouseDistance()
+    if (distance < mouse.radius) {
+      const forceDirectionX = dx / distance
+      const forceDirectionY = dy / distance
+
+      const acceleration = (mouse.radius - distance) / mouse.radius * 5
+
+      this.dx = forceDirectionX * acceleration
+      this.dy = forceDirectionY * acceleration
+    }
+
     if (this.x < 0 || (this.x + this.size) > canvas.width) {
       this.dx = -this.dx
     }
     if (this.y < 0 || (this.y + this.size) > canvas.height) {
       this.dy = -this.dy
     }
+  }
 
-    this.dx -= this.sx
-    this.dy -= this.sy
+  mouseDistance () {
+    const xDst = this.x - mouse.x
+    const yDst = this.y - mouse.y
+
+    return {
+      distance: Math.sqrt(xDst * xDst + yDst * yDst),
+      dx: xDst,
+      dy: yDst
+    }
   }
 }
 
 const particles = []
 
 const init = () => {
-  for (let x = 0; x < 100; x++) {
+  for (let x = 0; x < 500; x++) {
     const particle = new Particle({
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height
@@ -60,7 +89,7 @@ const init = () => {
 init()
 
 const connect = () => {
-  const maxDst = 150
+  const maxDst = 100
 
   particles.forEach(a => {
     particles.forEach(b => {
@@ -70,7 +99,7 @@ const connect = () => {
       const dst = Math.sqrt(xDst * xDst + yDst * yDst)
 
       if (dst < maxDst) {
-        ctx.globalAlpha = (maxDst - dst) / maxDst
+        ctx.globalAlpha =(maxDst - dst) / maxDst / 4
 
         ctx.beginPath()
         ctx.moveTo(a.x, a.y)
